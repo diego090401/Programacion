6d638f48c6b10fac17e22bf4153c0ae0b3fc9641
Diego Alonso Espinoza Rojas <deigoalonso0904@gmail.comm>
2021-08-06 13:10:50 -0600

    cambiar carpeta

--- Nombrenomediocre.py
+++
@@ -1,8 +0,0 @@
-Nombre = input("Ingrese su nombre y apellido: ")
-Nombre = list(Nombre)
-for i in range(len(Nombre)):
-    if Nombre[i] == " ":
-            Nombre[i] = "."
-        
-print(''.join(Nombre) + "@ccal.com")
-
