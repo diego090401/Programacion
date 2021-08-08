4bb096783c0935b6f4ba0f5cf81b3cff80895b3e
Diego Alonso Espinoza Rojas <deigoalonso0904@gmail.comm>
2021-08-07 18:11:25 -0600

    poo

---
+++ Aprendizaje/Platzi/POO/Python/DefinicioDeClase.py
@@ -0,0 +1,12 @@
+from POO.Python.Clase import Clase
+from Clase import Clase
+
+Diego = Clase()
+
+Diego.Edad = 19
+Diego.Nombre = "Diego"
+
+def run():
+    print(vars(Diego))
+if __name__ == "__main":
+    run()
