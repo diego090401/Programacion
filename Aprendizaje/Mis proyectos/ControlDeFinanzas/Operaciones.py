0d6294ac12b96c59a520ad69344a29120875c700
Diego Alonso Espinoza Rojas <deigoalonso0904@gmail.comm>
2021-08-12 16:04:40 -0600

    Proyecto de manejo de finanzas

---
+++ Aprendizaje/Mis proyectos/ControlDeFinanzas/Operaciones.py
@@ -0,0 +1,13 @@
+
+Text: list = []
+def Egress(Monto, Comentario) :
+    with open ("Aprendizaje\Mis proyectos\ControlDeFinanzas\Data.txt", 'r', encoding = "utf-8" ) as data:
+        for line in data : 
+            Text.append(line)
+            print(Text)
+def Ingress() : 
+    pass
+def SeeData() :
+    pass
+def EditData() :
+    pass
