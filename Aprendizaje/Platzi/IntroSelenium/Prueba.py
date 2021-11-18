e92fe2dd362c9a3a1a7ff98ce7cbc01bb5b521df
Diego Alonso Espinoza Rojas <diegoalonso0904@gmail.com>
2021-11-17 22:15:02 -0600

    Aprendizaje

--- Aprendizaje/Platzi/IntroSelenium/Prueba.py
+++ Aprendizaje/Platzi/IntroSelenium/Prueba.py
@@ -1,24 +1,26 @@
 from  selenium import webdriver
 from pyunitreport import HTMLTestRunner
 import unittest
-
 from selenium import webdriver
 
 class Prueba(unittest.TestCase):
+
     @classmethod
-    def setUpClass(cls) -> None:
+    def setUp(cls) -> None:
         cls.driver = webdriver.Chrome(executable_path=r"C:/Users/User/Desktop/chromedriver_win32/chromedriver.exe")
         driver =cls.driver
-        driver.implicitly_wait(10)
+        driver.get("https://blerning.colegiosaintjohn.com/index.php")
+       
         
     
     def test_find_field (self):
         driver = self.driver
-        driver.get("https://blerning.colegiosaintjohn.com/index.php")
         search_field = self.driver.find_element_by_id("login")
+    
     @classmethod
-    def tearDownClass(cls):
+    def tearDown(cls):
         driver =cls.driver
         driver.quit()
+
 if __name__ == "__main__":
 	unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output = 'reportes', report_name = 'hello-world-report'))
